75b0b1da86c4ef72a4d687ea1d48c5a31c6829df
Marcio Schlichting <marcioosergioo2015@gmail.com>
2023-06-08 17:24:50 -0300

    Feat: Creating user login service.

---
+++ Aldebaran.Accounts.Api/Controllers/AccountService.cs
@@ -0,0 +1,29 @@
+﻿using Aldebaran.Accounts.Repositories;
+
+namespace Aldebaran.Accounts.Api.Controllers;
+
+public class AccountService
+{
+    private readonly IUserRepository _userRepository;
+
+    public AccountService(IUserRepository userRepository)
+    {
+        _userRepository = userRepository;
+    }
+    
+    public async Task<string> Login(UserLoginCommand command)
+    {
+        var user = await _userRepository;
+        if (user == null)
+        {
+            throw new Exception("User not found");
+        }
+
+        if (!user.Password.Equals(command.Password))
+        {
+            throw new Exception("Invalid password");
+        }
+
+        return user;
+    }
+}
